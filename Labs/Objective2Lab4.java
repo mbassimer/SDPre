ceabd2c6e5ec4e7839397e7be601a954f941c186
mbassimer <51931871+mbassimer@users.noreply.github.com>
2019-07-06 13:43:53 -0600

    Add files via upload

--- Labs/Objective2Lab4.java
+++ Labs/Objective2Lab4.java
@@ -2,11 +2,10 @@ public class Objective2Lab4 {
   public static void main(String []  args) {
     double num1, num2, num3, average;
 
-    num1 = 10;
-    num2 = 15;
-    num3 = 12;
-    average = num1 + num2 + num3 / 3;
-
-    System.out.println (" " + average);
+    num1 = 15;
+    num2 = 6;
+    num3 = 2;
+    average = (num1 + num2 +num3) / 3;
+    System.out.println(average);
   }
 }
