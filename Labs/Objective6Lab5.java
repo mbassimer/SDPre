7d1a7d2703d51496e45ba556b91024ebde08d52c
mbassimer <51931871+mbassimer@users.noreply.github.com>
2019-07-08 06:01:05 -0600

    Add files via upload

---
+++ Labs/Objective6Lab5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Objective6Lab5 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while (true) {
+      System.out.println("____Menu____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite Foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+      selection = scanner.nextInt();
+
+      if(selection==1)
+      System.out.println("Hello World");
+      if(selecttion==2)
+      System.out.println("Apple,Banana,Coconut");
+      if(selection==3)
+      break;
+
+    }
+  }
+}
